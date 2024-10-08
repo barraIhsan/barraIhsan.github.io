72bd072cf45b2403e1740d494202d63a6c98f5c9
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2024-10-08 20:43:35 +0700

    feat: use scroll animation for tech stacks

---
+++ src/data/stacks.js
@@ -0,0 +1,23 @@
+import * as Icons from "simple-icons-astro";
+export const stacks = [
+  { Icon: Icons.Html5, name: "HTML" },
+  { Icon: Icons.Css3, name: "CSS" },
+  { Icon: Icons.Tailwindcss, name: "Tailwind" },
+  { Icon: Icons.Shadcnui, name: "shadcn/ui" },
+  { Icon: Icons.Javascript, name: "JavaScript" },
+  { Icon: Icons.Typescript, name: "TypeScript" },
+  { Icon: Icons.Astro, name: "Astro" },
+  { Icon: Icons.Svelte, name: "Svelte" },
+  { Icon: Icons.React, name: "React" },
+  { Icon: Icons.Nextdotjs, name: "Next.js" },
+  { Icon: Icons.Vite, name: "Vite" },
+  { Icon: Icons.Neovim, name: "Neovim" },
+  { Icon: Icons.Git, name: "Git" },
+  { Icon: Icons.Github, name: "GitHub" },
+  { Icon: Icons.Mariadb, name: "MariaDB" },
+  { Icon: Icons.Vercel, name: "Vercel" },
+  { Icon: Icons.Docker, name: "Docker" },
+  { Icon: Icons.Rust, name: "Rust" },
+  { Icon: Icons.Gnubash, name: "Bash" },
+  { Icon: Icons.Nodedotjs, name: "Node.js" },
+];
