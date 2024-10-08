3675fc5ba0170552a9d718b2d2145c51b71808d6
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2024-10-08 20:44:11 +0700

    feat: make projects section responsive and dynamic

---
+++ src/data/projects.js
@@ -0,0 +1,27 @@
+import * as Icons from "simple-icons-astro";
+export const projects = [
+  {
+    title: "AviaVentura",
+    desc: "AviaVentura is a company that provides aero agency",
+    link: "https://aviaventura.vercel.app",
+    featured: true,
+    stacks: [
+      {
+        Icon: Icons.Astro,
+        name: "Astro",
+      },
+      {
+        Icon: Icons.Tailwindcss,
+        name: "Tailwind",
+      },
+      {
+        Icon: Icons.Typescript,
+        name: "Typescript",
+      },
+      {
+        Icon: Icons.Sanity,
+        name: "Sanity",
+      },
+    ],
+  },
+];
