045496d1bace1d7bfbbd2a69ac8e75391d7ea5d8
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2024-10-10 13:37:32 +0700

    feat: add school projects to projects page

---
+++ src/data/schools.js
@@ -0,0 +1,135 @@
+import * as Icons from "simple-icons-astro";
+export const schools = [
+  {
+    title: "Weekly Assignment 1",
+    desc: "Our first weekly assignment was a simple form that had to be made with a simple HTML that followed HTML semantic.",
+    link: "https://barraihsan.github.io/tugas-kodein/weekly/1-form/",
+    small: true,
+    stacks: [
+      {
+        Icon: Icons.Html5,
+        name: "HTML",
+      },
+    ],
+  },
+  {
+    title: "Weekly Assignment 2",
+    desc: "We learnt CSS! Our next weekly assignment was making a single page login that utilized our CSS knowledge.",
+    link: "https://barraihsan.github.io/tugas-kodein/weekly/2-login/",
+    small: true,
+    stacks: [
+      {
+        Icon: Icons.Html5,
+        name: "HTML",
+      },
+      {
+        Icon: Icons.Css3,
+        name: "CSS",
+      },
+    ],
+  },
+  {
+    title: "Weekly Assignment 3",
+    desc: "Look, flexbox was here! In this assignment, we had to make a landing page for a product, in this case a Truck businesses.",
+    link: "https://barraihsan.github.io/tugas-kodein/weekly/3-landingPage/",
+    small: true,
+    stacks: [
+      {
+        Icon: Icons.Html5,
+        name: "HTML",
+      },
+      {
+        Icon: Icons.Css3,
+        name: "CSS",
+      },
+    ],
+  },
+  {
+    title: "Weekly Assignment 4",
+    desc: "Is it time to ditch writing manual CSS? Of course not! With bootstrap, we're able to make website faster than before!",
+    link: "https://barraihsan.github.io/tugas-kodein/weekly/4-bootstrapStore/",
+    small: true,
+    stacks: [
+      {
+        Icon: Icons.Html5,
+        name: "HTML",
+      },
+      {
+        Icon: Icons.Css3,
+        name: "CSS",
+      },
+      {
+        Icon: Icons.Bootstrap,
+        name: "Bootstrap",
+      },
+    ],
+  },
+  {
+    title: "Profile Cards",
+    desc: "A mini profile cards that describe me. Fun Fact: This project is used to be my homepage website.",
+    link: "https://barraihsan.github.io/tugas-kodein/daily/12-cards/",
+    small: true,
+    stacks: [
+      {
+        Icon: Icons.Html5,
+        name: "HTML",
+      },
+      {
+        Icon: Icons.Css3,
+        name: "CSS",
+      },
+    ],
+  },
+  {
+    title: "Umroh Plus Travel",
+    desc: "Learn animation for the first time, and we make this nice home/landing page for umroh travel",
+    link: "https://barraihsan.github.io/tugas-kodein/daily/13-animation/",
+    small: true,
+    stacks: [
+      {
+        Icon: Icons.Html5,
+        name: "HTML",
+      },
+      {
+        Icon: Icons.Css3,
+        name: "CSS",
+      },
+    ],
+  },
+  {
+    title: "Multimedia Club",
+    desc: "Make a simple landing page to polish our animation skill in css",
+    link: "https://barraihsan.github.io/tugas-kodein/daily/15-webKeyframe/",
+    small: true,
+    stacks: [
+      {
+        Icon: Icons.Html5,
+        name: "HTML",
+      },
+      {
+        Icon: Icons.Css3,
+        name: "CSS",
+      },
+    ],
+  },
+  {
+    title: "Semeru Adventure",
+    desc: "Now we learn about bootstrap and we make a landing page for Tidung Island, Ijen, and Bromo tour!",
+    link: "https://barraihsan.github.io/tugas-kodein/daily/16-bootstrapTrip/",
+    small: true,
+    stacks: [
+      {
+        Icon: Icons.Html5,
+        name: "HTML",
+      },
+      {
+        Icon: Icons.Css3,
+        name: "CSS",
+      },
+      {
+        Icon: Icons.Bootstrap,
+        name: "Bootstrap",
+      },
+    ],
+  },
+];
