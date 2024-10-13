8ec172db2b92fc498cacde909df4d3e08fd4f38a
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2024-10-13 20:22:11 +0700

    chore: include blog posts content to rss
    And also adapt it using content collection

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,12 +1,25 @@
-import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import sanitizeHtml from "sanitize-html";
+import MarkdownIt from "markdown-it";
+const parser = new MarkdownIt();
 
 export async function GET(context) {
+  const blogs = await getCollection("blogs");
   return rss({
     title: "barra's blogs",
     description:
       "where I talk about software development and my opinion around technology.",
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob("./blogs/*.mdx")),
+    items: blogs.map((blog) => ({
+      title: blog.data.title,
+      description: blog.data.desc,
+      pubDate: blog.data.pubDate,
+      link: `/blogs/${blog.slug}`,
+      content: sanitizeHtml(parser.render(blog.body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img", "h2"]),
+      }),
+    })),
     customData: `<language>en-US</language>`,
   });
 }
