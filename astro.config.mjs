c9afb736c135d620bc54ff3e59a89f92185682f6
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2024-10-15 09:25:16 +0700

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import tailwind from "@astrojs/tailwind";
 
 import mdx from "@astrojs/mdx";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({ applyBaseStyles: false }), mdx()],
+  integrations: [tailwind({ applyBaseStyles: false }), mdx(), sitemap()],
   site: "https://barraIhsan.github.io",
   base: "",
 });
