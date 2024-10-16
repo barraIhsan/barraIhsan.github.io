1c0aeb1e9c778685719fff8e6f83aaa5851ab663
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2024-10-16 12:42:58 +0700

    style: adjust tailwind typography styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -17,6 +17,41 @@ export default {
         scroll:
           "scroll var(--duration, 30s) var(--direction, forwards) linear infinite",
       },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            // make `inline code` have the same style as Github's
+            "code::before": {
+              content: "",
+            },
+            "code::after": {
+              content: "",
+            },
+            code: {
+              paddingInlineStart: ".4em",
+              paddingInlineEnd: ".4em",
+              paddingTop: ".2em",
+              paddingBottom: ".2em",
+              borderRadius: ".4em",
+              backgroundColor: theme("colors.gray.800"),
+            },
+            // no quote inside blockquote
+            "blockquote p:first-of-type::before": {
+              content: "",
+            },
+            "blockquote p:last-of-type::after": {
+              content: "",
+            },
+            // hover and already visited link style
+            "a:hover": {
+              color: theme("colors.gray.300"),
+            },
+            ":not(sup) > a:visited": {
+              color: theme("colors.gray.400"),
+            },
+          },
+        },
+      }),
     },
   },
   plugins: [
